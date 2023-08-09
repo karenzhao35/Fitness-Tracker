257f33489f2176b22e3cd26243278836ec6b77d3
karenzhao35 <karenzhao35@gmail.com>
2023-08-08 23:31:35 -0700

    fixed event code coverage

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -1,19 +1,22 @@
 package model;
 
 
+import model.food.Meals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the Event class
  */
 public class EventTest {
-    private Event e;
+    private Event e1;
+    private Event e2;
+    private Event e3;
     private Date d;
 
     //NOTE: these tests might fail if time at which line (2) below is executed
@@ -22,19 +25,38 @@ public class EventTest {
 
     @BeforeEach
     public void runBefore() {
-        e = new Event("Sensor open at door");   // (1)
+        e1 = new Event("Sensor open at door");   // (1)
+        e2 = new Event("Sensor open at door");
+        e3 = new Event("Sensor closed at door");
+
         d = Calendar.getInstance().getTime();   // (2)
     }
 
     @Test
     public void testEvent() {
-        assertEquals("Sensor open at door", e.getDescription());
-        assertEquals(d, e.getDate());
+        assertEquals("Sensor open at door", e1.getDescription());
+        assertEquals(d, e1.getDate());
     }
 
     @Test
     public void testToString() {
-        assertEquals(d.toString() + "\n" + "Sensor open at door", e.toString());
+        assertEquals(d.toString() + "\n" + "Sensor open at door", e1.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        assertTrue(e1.equals(e2));
+        assertFalse(e1.equals(e3));
+        assertFalse(e1.equals(null));
+        assertFalse(e2.equals(new Meals()));
+    }
+
+    @Test
+    public void testHashCode() {
+        assertTrue(e1.hashCode() == e2.hashCode());
+        assertFalse(e1.hashCode() == e3.hashCode());
+        Meals m = new Meals();
+        assertFalse(e2.hashCode() == m.hashCode());
     }
 }
 
