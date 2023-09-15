713d55e18717d625e8de6aa998daf4ce4b4be992
karenzhao35 <karenzhao35@gmail.com>
2023-09-14 22:06:29 -0700

    Phase 4: Task 3 (improved again)

--- src/main/ui/ColourPicker.java
+++ src/main/ui/ColourPicker.java
@@ -7,6 +7,8 @@ public class ColourPicker {
     public static final Color TEXT_COLOR = new Color(0x4F5B67);
     // dark green
     public static final Color SIDEBAR = new Color(0x506A67);
+    // public static final Color SIDEBAR = new Color(0xC6C6BB);
+
     // WHITE
     public static final Color MAIN_COLOUR = new Color(0xEEECE6);
 
